10a10ee2bed31558d1e3b511b8692428529a61ff
Moamen Ayman <senkotoka@gmail.com>
2025-08-10 00:45:06 +0300

    change theme and create notes view

---
+++ lib/views/notes_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class NotesView extends StatelessWidget {
+  const NotesView({super.key});
+
+  static String id = 'HomeScreen';
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}

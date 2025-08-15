09e1cbed4a35b18a815dfbd3f4506bdee2e70558
Moamen Ayman <senkotoka@gmail.com>
2025-08-15 22:49:03 +0300

    delete note

--- lib/widgets/custom_note_item.dart
+++ lib/widgets/custom_note_item.dart
@@ -45,7 +45,9 @@ class CustomNoteItem extends StatelessWidget {
                   ),
                 ),
                 trailing: IconButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    note.delete();
+                  },
                   icon: const Icon(
                     FontAwesomeIcons.trash,
                     color: Colors.black,
